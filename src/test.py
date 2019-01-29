841a03ea66d1ec306ce31338daf3779eb7275c3b
james <james-po@is.naist.jp>
2019-01-29 16:26:27 +0900

    better real offset sans FT sensor

--- src/test.py
+++ src/test.py
@@ -8,12 +8,17 @@ from tf.transformations import quaternion_from_euler
 rospy.init_node('thread_test_node', anonymous=True)
 
 # grab an object
-print('  Grabbing something ...')
 rospy.wait_for_service('ur5/command/moveTo')
 m2_ = rospy.ServiceProxy('ur5/command/moveTo', MoveTo)
 
-pose_tuple_ = [ 0.5, 0.0, 0.1 ] + list(quaternion_from_euler(0,1.5708,0)) + [ 10.0 ]
+pose_tuple_ = [ 0.5, -0.2, 0.0 ] + list(quaternion_from_euler(0,1.5708,0)) + [ 5.0 ]
+m2_(MoveToRequest(ur_state=pose_tuple_, gripper_state=True))
+
+pose_tuple_[2] = 0.2
+m2_(MoveToRequest(ur_state=pose_tuple_, gripper_state=True))
+
+pose_tuple_[1] = 0.2
+m2_(MoveToRequest(ur_state=pose_tuple_, gripper_state=True))
+
+pose_tuple_[2] = 0.0
 m2_(MoveToRequest(ur_state=pose_tuple_, gripper_state=False))
-
-print('  n u t')
-
